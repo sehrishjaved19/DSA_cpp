d093442ce499e56afac563d0df594a9b479c3e6e
Sehrish <sehrishjaved119@gmail.com>
2026-01-05 04:01:08 +0500

    Added 03_insertion_at_end_of_array

---
+++ 01_introduction_to_arrays/03_insertion_at_end_of_array.cpp
@@ -0,0 +1,49 @@
+/*
+Algorithm - Insertion at end
+1- START
+2- REPEAT step-3 TO 4 FOR I = 3 TO 9 BY 1
+3-  INPUT values in N
+4-   ABC[I] = N
+   [End STEP-2 loop]
+5-END
+*/
+
+// Program to find out the even values of the array
+#include<iostream>
+using namespace std;
+class Temp {
+    private:
+    int abc[10];
+    public:
+
+    int assign() //member function to input data into array
+    {
+        abc[0] = 66;
+        abc[1] = 70;
+        abc[2] = 36;  
+    }
+    void insert()
+    {
+        cout<<"Enter 7 values & press Enter after typing each value\n";
+        for(int i = 3; i<=9; i++)
+        {
+            cin>>abc[i];
+        }
+    }
+    void display()
+    {
+        for(int i = 0; i<=9; i++)
+        {
+            cout<<abc[i]<<endl;
+        }
+    }
+};
+int main()
+{
+    Temp obj;
+    obj.assign();
+    obj.insert();
+    cout<<"Values in Array "<<endl;
+    obj.display();
+    return 0;
+}
