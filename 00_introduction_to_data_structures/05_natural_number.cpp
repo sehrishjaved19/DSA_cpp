ff127262ff3bdb4a4e596b073b8ae099cbaec497
Sehrish <sehrishjaved119@gmail.com>
2026-01-04 20:04:28 +0500

    Added 05_natural_number

---
+++ 00_introduction_to_data_structures/05_natural_number.cpp
@@ -0,0 +1,20 @@
+/* 
+Algorithm - NATURAL NUMBER
+1- START
+2- REPEAT Step-3 FOR C=1 TO 10 BY 1
+3- PRINT C
+[End of step-2 loop]
+4- END
+*/
+
+//Program to display first 10 natural numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int c=1; c<=10; c++)
+    {
+        cout<<c<<endl;
+    }
+    return 0;
+}
